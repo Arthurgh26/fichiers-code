9a38431d175128e02dd053fadf3c0abf53a92814
Arthurgh26 <arthur.noelespitalier@gmail.com>
2026-03-21 18:58:05 +0100

    semaine2python

---
+++ jour11python.py
@@ -0,0 +1,11 @@
+client_solaire = {
+    "nom" : "Arthur",
+    "budget" : 1200.0,
+    "panneaux" : 4,
+    "acttif" : True,
+    "rendement" : 0.18
+}
+
+print(f"Client : {client_solaire['nom']}")
+print(f"Nombre de panneaux : {client_solaire['panneaux']}")
+print(f"Rendement : {client_solaire['rendement']}")
